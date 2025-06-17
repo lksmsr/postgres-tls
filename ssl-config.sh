6a8bcc2ec65d4b5c7a77891c9d921ef3daa0a842
Lukas Mauser <l.mauser@wimadev.de>
2025-06-17 14:46:34 +0200

    Update ssl-config.sh

--- ssl-config.sh
+++ ssl-config.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "START CONFIG SCRIPT"
+
 echo "$SERVER_CRT" > /var/lib/postgresql/server.crt
 echo "$SERVER_KEY" > /var/lib/postgresql/server.key
 echo "$ROOT_CA_CRT" > /var/lib/postgresql/rootCA.crt
+
+
+echo $SERVER_CRT $SERVER_KEY $ROOT_CA_CRT
+
 
 chmod 600 /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
 chown postgres:postgres /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
