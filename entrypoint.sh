02512bec60b316178319dd87e3557d4196e30770
Lukas Mauser <l.mauser@wimadev.de>
2025-06-18 15:37:33 +0200

    Update and rename ssl-config.sh to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+set -e
 
 # Add certificate files
 echo "$SERVER_CRT" > /var/lib/postgresql/server.crt
 echo "$SERVER_KEY" > /var/lib/postgresql/server.key
 echo "$ROOT_CA_CRT" > /var/lib/postgresql/rootCA.crt
 
-# Set permissions for the certificate files
+# Update file permissions of certificates
 chmod 600 /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
 chown postgres:postgres /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
-
 
 # Configure PostgreSQL to use SSL
 echo "ssl = on" >> /var/lib/postgresql/data/postgresql.conf
@@ -16,5 +16,8 @@ echo "ssl_cert_file = '/var/lib/postgresql/server.crt'" >> /var/lib/postgresql/d
 echo "ssl_key_file = '/var/lib/postgresql/server.key'" >> /var/lib/postgresql/data/postgresql.conf
 echo "ssl_ca_file = '/var/lib/postgresql/rootCA.crt'" >> /var/lib/postgresql/data/postgresql.conf
 
-# Force SSL
+# Enforce SSL for all connections
 echo "hostssl all all all cert clientcert=verify-full" > /var/lib/postgresql/data/pg_hba.conf
+
+# Run the base entrypoint
+docker-entrypoint.sh postgres
