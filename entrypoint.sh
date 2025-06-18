217128366514863f53ede3029a3f6d5bae730b39
Lukas Mauser <l.mauser@wimadev.de>
2025-06-18 16:10:37 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,18 @@ echo "$ROOT_CA_CRT" > /var/lib/postgresql/rootCA.crt
 chmod 600 /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
 chown postgres:postgres /var/lib/postgresql/server.* /var/lib/postgresql/rootCA.crt
 
-# Configure PostgreSQL to use SSL
-echo "ssl = on" >> /var/lib/postgresql/data/postgresql.conf
-echo "ssl_cert_file = '/var/lib/postgresql/server.crt'" >> /var/lib/postgresql/data/postgresql.conf
-echo "ssl_key_file = '/var/lib/postgresql/server.key'" >> /var/lib/postgresql/data/postgresql.conf
-echo "ssl_ca_file = '/var/lib/postgresql/rootCA.crt'" >> /var/lib/postgresql/data/postgresql.conf
+# Only write to the data dir if it is not empty
+if [ "$(ls -A /var/lib/postgresql/data)" ]; then
+  # Configure PostgreSQL to use SSL
+  echo "ssl = on" >> /var/lib/postgresql/data/postgresql.conf
+  echo "ssl_cert_file = '/var/lib/postgresql/server.crt'" >> /var/lib/postgresql/data/postgresql.conf
+  echo "ssl_key_file = '/var/lib/postgresql/server.key'" >> /var/lib/postgresql/data/postgresql.conf
+  echo "ssl_ca_file = '/var/lib/postgresql/rootCA.crt'" >> /var/lib/postgresql/data/postgresql.conf
+
+  # Enforce SSL for all connections
+  echo "hostssl all all all cert clientcert=verify-full" > /var/lib/postgresql/data/pg_hba.conf
+fi
+
 
 # Enforce SSL for all connections
 echo "hostssl all all all cert clientcert=verify-full" > /var/lib/postgresql/data/pg_hba.conf
